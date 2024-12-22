259bb41a331374e8bdd33e07a048785dac1c943f
AjeetY28 <ajeet.bca28@gmail.com>
2024-12-22 22:56:31 +0530

    create MessagRequest

--- src/main/java/com/substring/chat/RoomRepository.java
+++ src/main/java/com/substring/chat/RoomRepository.java
@@ -1,5 +1,10 @@
 package com.substring.chat;
 
-public class RoomRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-}
+import com.substring.chat.entities.Room;
+
+public interface RoomRepository extends MongoRepository<Room, String>{
+
+    Room findByRoomId(String roomId);
+} 
