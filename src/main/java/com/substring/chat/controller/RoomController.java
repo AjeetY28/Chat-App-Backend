a145f6ca32fc459112ef821c763b7a35e190e436
AjeetY28 <ajeet.bca28@gmail.com>
2024-12-22 16:14:50 +0530

    added get message feature

--- src/main/java/com/substring/chat/controller/RoomController.java
+++ src/main/java/com/substring/chat/controller/RoomController.java
@@ -1,5 +1,7 @@
 package com.substring.chat.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.substring.chat.entities.Message;
 import com.substring.chat.entities.Room;
 import com.substring.chat.repositories.RoomRepository;
 
@@ -53,6 +57,29 @@ public class RoomController {
             return ResponseEntity.badRequest().body("Room not found");
         }
         return ResponseEntity.ok(room);
+    }
+    //get message of room
+     @GetMapping("/{roomId}/messages")
+    public ResponseEntity<List<Message>> getMessaages(
+        @PathVariable String roomId,
+        @RequestParam(value="page",defaultValue="0",required=false) int page,
+        @RequestParam(value="size",defaultValue="20",required = false) int size
+    )
+    {
+       Room room= roomRepository.findByRoomId(roomId);
+        if(room==null)
+        {
+            return ResponseEntity.badRequest().build();
+        }
+
+        List<Message> messages=room.getMessages();
+
+        int start=Math.max(0,messages.size()-(page+1)*size);
+
+        int end=Math.min(messages.size(),start+ size);
+
+        List<Message> paginatedMessages = messages.subList(start, end);
+        return ResponseEntity.ok(messages);
     }
 
 }
